7a84d7d896b618c136d24cb6622da5a574d1adea
Gavin Bedi <92979117+GavinBedi@users.noreply.github.com>
2026-07-25 21:21:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack<char> st;
+       int i;
+      
+       for(i=0;i<s.size();i++)
+       {
+        if(s[i]=='('||s[i]=='{'||s[i]=='[')
+            st.push(s[i]);
+            else
+            {
+                if(st.empty() or (st.top()=='('&& s[i]!=')')||(st.top()=='['&&s[i]!=']')||(st.top()=='{'&&s[i]!='}'))
+                return false;
+                st.pop();
+            }
+       }
+       return st.empty();
+    }
+};
