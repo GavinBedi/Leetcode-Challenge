a6d3510fd15125ed0fbf90258ae4638c4f662c65
Gavin Bedi <92979117+GavinBedi@users.noreply.github.com>
2026-07-28 13:18:36 +0530

    Time: 72 ms (42.64%), Space: 54.1 MB (95.38%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+       int l=s.size();
+       int i;
+       sort(begin(s),begin(s)+(l/2));
+       for(i=0;i<l/2;i++)
+       s[l-1-i]=s[i];
+       return s;
+
+
+
+    }
+};
